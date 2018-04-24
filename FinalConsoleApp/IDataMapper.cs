730d03d6353a5bda8011c29a64b7c49091ff3c88
Marc Williams <oti_instructor@hotmail.com>
2018-04-24 08:16:56 -0300

    Added comments to explain interfaces

--- FinalConsoleApp/IDataMapper.cs
+++ FinalConsoleApp/IDataMapper.cs
@@ -4,6 +4,16 @@ using System.Text;
 
 namespace FinalConsoleApp
 {
+    /*
+     * - Interfaces in C# provide a way to achieve run-time polymorphism.
+     * - An Interface is defined as a syntactical contract that all the 
+     *   classes inheriting the interface should follow.
+     * - The interface defines the 'WHAT' part of the syntactical contract
+     *   and the deriving classes define the 'HOW' part.
+     * - Interfaces define properties, methods, and events which are members
+     *   of the interface
+     * - Interfaces are the ultimate abstraction.
+     * /
     internal interface IDataMapper<T>
     {
         /*
